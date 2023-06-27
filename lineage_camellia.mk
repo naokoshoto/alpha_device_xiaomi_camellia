9fcf07c00472b3f21f361612f0ee4b8c29f9adb6
Naoko Shoto <mdigreget@gmail.com>
2023-06-27 21:11:10 +0700

    camellia: Adapt to AlphaDroid requirements

--- lineage_camellia.mk
+++ lineage_camellia.mk
@@ -9,18 +9,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from camellia device
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
-# PixelExperience Stuff
+# Alphadroid Stuff
+IS_PHONE := true
+USE_PIXEL_CHARGING := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+TARGET_ENABLE_BLUR := false
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_DEFAULT_ADB_ENABLED := true
+
+# Google Apps Stuffs
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_USE_GOOGLE_TELEPHONY := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_camellia
+PRODUCT_NAME := lineage_camellia
 PRODUCT_DEVICE := camellia
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := Redmi
@@ -28,3 +45,6 @@ PRODUCT_MODEL := camellia
 
 # Build info
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+ALPHA_MAINTAINER=NaokoShoto
+
