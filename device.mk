ef88f43510a763e45d2a35d674dce78b695c3541
Naoko Shoto <mdigreget@gmail.com>
2023-12-04 21:27:28 +0700

    camellia: overlays: Configure aux camera for Aperture

--- device.mk
+++ device.mk
@@ -314,6 +314,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureOverlayCamellia \
     CarrierConfigOverlayCamellia \
     FrameworksResOverlayCamellia \
     SettingsOverlayCamellia \
