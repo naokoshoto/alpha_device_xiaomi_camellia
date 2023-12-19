6341524f5f8a90f4b5b4227fef415fc1c2e20cc9
bengris32 <bengris32@protonmail.ch>
2023-12-19 12:55:06 +0000

    camellia: Update IMS from plato V14.0.7.0.ULQMIXM
    Change-Id: I9ab402b7d2bdacba06caaabda18e70c9a8189ae4
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.sh
+++ extract-files.sh
@@ -74,6 +74,9 @@ function blob_fixup {
         vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
             sed -i 's/start/enable/' "$2"
             ;;
+        system_ext/lib64/libsink.so)
+            "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
+            ;;
 	vendor/lib*/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
